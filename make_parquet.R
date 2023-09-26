337a82e4015620cae4d3925cf17f7f7a32f705a8
Ian Gow <iandgow@gmail.com>
2023-09-26 19:29:39 +1000

    Further tweaks to make_parquet.R.
     - Relates to #5.

--- make_parquet.R
+++ make_parquet.R
@@ -2,9 +2,7 @@ library(tidyverse)
 library(duckdb)
 
 db <- dbConnect(duckdb::duckdb())
-dbExecute(db, "CREATE TABLE movies AS SELECT * FROM read_csv_auto('archive/rotten_tomatoes_movies.csv')")
-dbExecute(db, "CREATE TABLE reviews AS SELECT * FROM read_csv_auto('archive/rotten_tomatoes_movie_reviews.csv')")
-dbExecute(db, "COPY movies TO 'archive/movies.parquet' (FORMAT PARQUET)")
-dbExecute(db, "COPY reviews TO 'archive/reviews.parquet' (FORMAT PARQUET)")
+dbExecute(db, "COPY (SELECT * FROM read_csv_auto('archive/rotten_tomatoes_movies.csv')) TO 'archive/movies.parquet' (FORMAT PARQUET)")
+dbExecute(db, "COPY (SELECT * FROM read_csv_auto('archive/rotten_tomatoes_movie_reviews.csv')) TO 'archive/reviews.parquet' (FORMAT PARQUET)")
 
 dbDisconnect(db, shutdown = TRUE)
